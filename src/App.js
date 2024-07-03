34606f59da42d83f907fdb15d14d9eb7343703b3
sagun900 <sagunk268@gmail.com>
2024-07-03 07:56:34 +0545

    new bramch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 import React from 'react'
 
-import Displaysec from './pages/Home/Displaysec'
-import Header from './pages/Home/Header'
-import Tech from './pages/Home/Tech'
-import Intro from './pages/Intro'
-import Footer from './pages/Home/Footer'
-
-
 const App = () => {
   return (
     <div>
-
-
-      <Header />
-      <Displaysec />
-      <Tech />
-      <Intro />
-      <Footer />
 
 
     </div>
